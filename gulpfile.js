6c70aeec53ccf01d2c76b39047136b3f974258fe
Joe Burdick <josephdburdick@gmail.com>
2014-09-24 20:51:54 -0400

    Bump gulp-useref version, update syntax.

--- gulpfile.js
+++ gulpfile.js
@@ -33,16 +33,16 @@ gulp.task('views', function () {
 gulp.task('html', ['views', 'styles', 'scripts'], function () {
     var jsFilter = $.filter('**/*.js');
     var cssFilter = $.filter('**/*.css');
-
+    var assets = $.useref.assets({searchPath: '{.tmp,app}'});
     return gulp.src('.tmp/*.html')
-        .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
+        .pipe(assets)
         .pipe(jsFilter)
         .pipe($.uglify())
         .pipe(jsFilter.restore())
         .pipe(cssFilter)
         .pipe($.csso())
         .pipe(cssFilter.restore())
-        .pipe($.useref.restore())
+        .pipe(assets.restore())
         .pipe($.useref())
         .pipe(gulp.dest('dist'))
         .pipe($.size());
